9b7e5be6afeabf204d2062fdfbaf8aa23800fce2
CodyDillinger-Ubuntu16-PC <Cody.M.Dillinger@gmail.com>
2020-08-11 13:31:31 -0400

    adding Gazebo simulator code for simple i-robot position controlling

---
+++ Gazebo_Simulator/i_robot_simulator/build/gtest/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gtest
+# Build directory: /home/codymdillinger/Documents/NRSL/Robotics/Gazebo_Simulator/i_robot_simulator/build/gtest/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ Gazebo_Simulator/i_robot_simulator/build/iRobot_create/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/codymdillinger/Documents/NRSL/Robotics/Gazebo_Simulator/i_robot_simulator/src/iRobot_create
+# Build directory: /home/codymdillinger/Documents/NRSL/Robotics/Gazebo_Simulator/i_robot_simulator/build/iRobot_create
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
